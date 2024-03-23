054ad4d513f72e8af0448992f60149f1a13f22f7
peterfulop <mrpeterfulop@gmail.com>
2024-03-23 19:47:09 +0100

    Init application

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Page = () => {
+    return (
+        <View>
+            <Text>index</Text>
+        </View>
+    );
+};
+
+export default Page;
